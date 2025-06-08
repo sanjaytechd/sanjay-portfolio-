e1526605cdc59be6d6153962942b429019d57ca8
SAGAR <Sagarnbharadwaj111@gmail.com>
2025-06-08 22:52:48 +0530

    Update Flask app to use Render port and 0.0.0.0 host

--- gemini_api.py
+++ gemini_api.py
@@ -98,5 +98,5 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    # Make sure templates folder exists in the same directory as this script
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get port from Render or default 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
